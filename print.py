eeb86a02fec3aa8afd39c60750abdd87e11dd7fb
Amirhossein <167887406+AmirsMoradi@users.noreply.github.com>
2024-08-19 15:07:46 +0330

    Add files via upload

---
+++ print.py
@@ -0,0 +1,17 @@
+name = "Amir"
+age = 23
+formatted_string = "Name: {}, Age: {}".format(name, age) 
+print(formatted_string) 
+
+
+name = "Amir"
+age = 23
+formatted_string = f"Name: {name}, Age: {age}" 
+print(formatted_string) 
+
+
+
+name = "Amir"
+age = 23
+formatted_string = "name: %s، age: %d" % (name,age)
+print(formatted_string)
